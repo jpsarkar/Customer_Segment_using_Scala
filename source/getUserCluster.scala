9375af65647d80a09959c7b518ead22218bf87e7
Jnanendra Sarkar <jpsarkar@users.noreply.github.com>
2017-10-23 21:07:34 +0530

    Update getUserCluster.scala

--- source/getUserCluster.scala
+++ source/getUserCluster.scala
@@ -4,26 +4,26 @@ import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.ml.linalg.Vectors
 
 
-val data = sc.textFile("telecomdata.csv")
+val data = sc.textFile("sample.csv")
 val parsedData = data.map(s => s.split(',')).map(f => (f(3).toDouble,f(4).toDouble)).filter(p => !((p._1 == 0) && (p._2 == 0))).persist()
 
 val RowsDF = parsedData.toDF("latitude", "longitude")
 
 val assembler = new VectorAssembler().setInputCols(Array("latitude", "longitude")).setOutputCol("features")
-val telecomDF = assembler.transform(RowsDF).select("features")
+val sampleDF = assembler.transform(RowsDF).select("features")
 
 val kmeans = new KMeans().setK(3).setSeed(1L).setPredictionCol("prediction")
 
-val model = kmeans.fit(telecomDF)
+val model = kmeans.fit(sampleDF)
 
 
-val WSSSE = model.computeCost(telecomDF)
+val WSSSE = model.computeCost(sampleDF)
 println(s"Within Set Sum of Squared Errors = $WSSSE")
 
 println("Cluster Centers: ")
 model.clusterCenters.foreach(println)
 
-val predictionResult = model.transform(telecomDF)
+val predictionResult = model.transform(sampleDF)
 predictionResult.rdd.coalesce(1, true).saveAsTextFile("output")
 
 System.exit(0)
